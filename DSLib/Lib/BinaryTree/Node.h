3289e87295d292417172573d558ca7ddaa88a8ef
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-11-18 13:33:54 +0530

    Revert back to simple node

--- DSLib/Lib/BinaryTree/Node.h
+++ DSLib/Lib/BinaryTree/Node.h
@@ -12,56 +12,15 @@ namespace ds
 		{
 		public:
 			virtual ~node() = default;
-			explicit node(T data) :data(data) {}
-
-			virtual node_type get_node_type() = 0;
+			explicit node(T data, const node_type node_type) : data(data), node_type(node_type)
+			{
+			}
 
 			T data;
+			node_type node_type;
 			std::weak_ptr<node> ancestor;
 			std::shared_ptr<node> left_child;
 			std::shared_ptr<node> right_child;
-		};
-
-		template<typename T>
-		class root_node : public node<T>
-		{
-		public:
-			explicit root_node(const T& data)
-				: node<T>(data), node_type(node_type::k_root)
-			{
-			}
-
-			node_type get_node_type() override { return node_type; }
-
-			node_type node_type;
-		};
-
-		template<typename T>
-		class right_node : public node<T>
-		{
-		public:
-			explicit right_node(const T& data)
-				: node<T>(data), node_type(node_type::k_right_child)
-			{
-			}
-
-			node_type get_node_type() override { return node_type; }
-
-			node_type node_type;
-		};
-
-		template<typename T>
-		class left_node : public node<T>
-		{
-		public:
-			explicit left_node(const T& data)
-				: node<T>(data), node_type(node_type::k_left_child)
-			{
-			}
-
-			node_type get_node_type() override { return node_type; }
-
-			node_type node_type;
 		};
 	}
 }
