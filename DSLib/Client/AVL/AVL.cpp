9068d608470b56b2736931f4389b4e8cef12f205
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-11-25 19:57:48 +0530

    Document find_imbalance

--- DSLib/Client/AVL/AVL.cpp
+++ DSLib/Client/AVL/AVL.cpp
@@ -5,13 +5,39 @@ int main(int argc, char* argv[])
 {
 	ds::avl<double> avl;
 	std::vector<double> data{ 3, 2, 4, 5, 1 };
-	for (auto &item: data)
+	for (auto &item : data)
 	{
 		avl.insert(item);
 	}
-	
-	double i = 0;
-	avl.insert(i);
+
+	double i = 4.7;
+	const auto insert = avl.insert(i);
 	avl.inorder();
+	/*ds::imbalance_info<double> info;
+	if (avl.find_imbalance(insert, &info))
+	{
+		std::cout << "Imbalance:" << std::endl;
+		std::cout << "z: " << info.z->data << std::endl;
+		std::cout << "y: " << info.y->data << std::endl;
+		std::cout << "x: " << info.x->data << std::endl;
+		std::cout << "Imbalance config: ";
+		switch (info.config) {
+		case ds::imbalance_config::k_ll: 
+			std::cout << "ll";
+			break;
+		case ds::imbalance_config::k_lr: 
+			std::cout << "lr";
+			break;
+		case ds::imbalance_config::k_rl: 
+			std::cout << "rl";
+			break;
+		case ds::imbalance_config::k_rr: 
+			std::cout << "rr";
+			break;
+		}
+
+		std::cout << std::endl;
+	}*/
+
 	return 0;
 }
--- DSLib/Lib/AVL/AVL.h
+++ DSLib/Lib/AVL/AVL.h
@@ -8,9 +8,13 @@ namespace ds
 	template<class T>
 	struct imbalance_info
 	{
+		// Node z is where the imbalance starts while traversing upwards from the inserted node
 		std::shared_ptr<bin_tree::node<T>> z;
+		// Node y is the child of z with a greater height
 		std::shared_ptr<bin_tree::node<T>> y;
+		// Node x is the child of y with a greater height
 		std::shared_ptr<bin_tree::node<T>> x;
+		// Denotes the configuration of z, y and x nodes
 		imbalance_config config;
 	};
 
@@ -21,6 +25,7 @@ namespace ds
 		std::shared_ptr<bin_tree::node<T>> insert(T& key) override;
 
 	private:
+		// Finds imbalance from the node n, imbalance info is provided as an out parameter
 		bool find_imbalance(const std::shared_ptr<bin_tree::node<T>> &n, imbalance_info<T> *out_info);
 	};
 
@@ -41,17 +46,26 @@ namespace ds
 		auto left_child_height = bst<T>::height(n->left_child);
 		auto right_child_height = bst<T>::height(n->right_child);
 		auto height_diff = left_child_height - right_child_height;
+
+		// If the height difference between left and right children is 0, 1 or -1, there is no imbalance
+		// Proceed to check if there is any imbalance in the ancestor node
 		if (height_diff == 0 || height_diff == 1 || height_diff == -1)
 		{
-			return find_imbalance(n->ancestor, out_info);
+			return find_imbalance(n->ancestor.lock(), out_info);
 		}
 
+		// Otherwise, there is some imbalance
+		// The node with imbalance is denoted as z
 		out_info->z = n;
 		if (height_diff > 0)
 		{
+			// Since we subtract right child's height from left child's height, 
+			// if the difference is positive, then left child has a greater height compared to right child
+			// Child of z with the greater height is denoted as y
 			out_info->y = n->left_child;
 			if (bst<T>::height(out_info->y->left_child) >= bst<T>::height(out_info->y->right_child))
 			{
+				// x is child of y which has the longer path
 				out_info->x = out_info->y->left_child;
 				out_info->config = imbalance_config::k_ll;
 			}
