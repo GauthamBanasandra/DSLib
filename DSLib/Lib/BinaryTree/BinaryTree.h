5f3442df8db6a191ab6f2ef3eb21ae19c5ba3f9f
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-11-27 20:37:57 +0530

    Move BinaryTree under bin_tree namespace

--- DSLib/Lib/BinaryTree/BinaryTree.h
+++ DSLib/Lib/BinaryTree/BinaryTree.h
@@ -7,110 +7,113 @@
 
 namespace ds
 {
-	template<class T>
-	class binary_tree
+	namespace bin_tree
 	{
-		using big_int = unsigned long long;
-
-	public:
-		explicit binary_tree() : node_count(0) {}
-		virtual ~binary_tree() = default;
-
-		// Traversal methods with visit_action as a lambda
-		void preorder(std::function<void(T &data)> visit_action = [](T &data)->void {std::cout << data << '\n'; });
-		void postorder(std::function<void(T &data)> visit_action = [](T &data)->void {std::cout << data << '\n'; });
-		void inorder(std::function<void(T &data)> visit_action = [](T &data)->void {std::cout << data << '\n'; });
-
-		// C++ doesn't allow a member to be used as the default value (for node as root)
-		// Thus, we need expose traversal methods as wrappers of these functions
-		void preorder(bin_tree::node<T> *n, std::function<void(T &data)> &&visit_action);
-		void postorder(bin_tree::node<T> *n, std::function<void(T &data)> &&visit_action);
-		void inorder(bin_tree::node<T> *n, std::function<void(T &data)> &&visit_action);
-		long long height();
-		long long height(const std::shared_ptr<bin_tree::node<T>> &n);
-
-		// Insert and remove methods are pure virtual (derived class must implement them)
-		virtual std::shared_ptr<bin_tree::node<T>> insert(T& key) = 0;
-		virtual bool remove(T key) = 0;
-
-		std::shared_ptr<bin_tree::node<T>> root;
-		big_int node_count;
-	};
-
-	template <class T>
-	void binary_tree<T>::preorder(std::function<void(T &data)> visit_action)
-	{
-		preorder(root.get(), std::move(visit_action));
-	}
-
-	template <class T>
-	void binary_tree<T>::postorder(std::function<void(T &data)> visit_action)
-	{
-		postorder(root.get(), std::move(visit_action));
-	}
-
-	template <class T>
-	void binary_tree<T>::inorder(std::function<void(T &data)> visit_action)
-	{
-		inorder(root.get(), std::move(visit_action));
-	}
-
-	template <class T>
-	void binary_tree<T>::preorder(bin_tree::node<T>* n, std::function<void(T& data)> &&visit_action)
-	{
-		if (n == nullptr)
+		template<class T>
+		class binary_tree
 		{
-			return;
+			using big_int = unsigned long long;
+
+		public:
+			explicit binary_tree() : node_count(0) {}
+			virtual ~binary_tree() = default;
+
+			// Traversal methods with visit_action as a lambda
+			void preorder(std::function<void(T &data)> visit_action = [](T &data)->void {std::cout << data << '\n'; });
+			void postorder(std::function<void(T &data)> visit_action = [](T &data)->void {std::cout << data << '\n'; });
+			void inorder(std::function<void(T &data)> visit_action = [](T &data)->void {std::cout << data << '\n'; });
+
+			// C++ doesn't allow a member to be used as the default value (for node as root)
+			// Thus, we need expose traversal methods as wrappers of these functions
+			void preorder(node<T> *n, std::function<void(T &data)> &&visit_action);
+			void postorder(node<T> *n, std::function<void(T &data)> &&visit_action);
+			void inorder(node<T> *n, std::function<void(T &data)> &&visit_action);
+			long long height();
+			long long height(const std::shared_ptr<node<T>> &n);
+
+			// Insert and remove methods are pure virtual (derived class must implement them)
+			virtual std::shared_ptr<node<T>> insert(T& key) = 0;
+			virtual bool remove(T key) = 0;
+
+			std::shared_ptr<node<T>> root;
+			big_int node_count;
+		};
+
+		template <class T>
+		void binary_tree<T>::preorder(std::function<void(T &data)> visit_action)
+		{
+			preorder(root.get(), std::move(visit_action));
 		}
 
-		visit_action(n->data);
-		preorder(n->left_child.get(), std::move(visit_action));
-		preorder(n->right_child.get(), std::move(visit_action));
-	}
-
-	template <class T>
-	void binary_tree<T>::postorder(bin_tree::node<T>* n, std::function<void(T& data)>&& visit_action)
-	{
-		if (n == nullptr)
+		template <class T>
+		void binary_tree<T>::postorder(std::function<void(T &data)> visit_action)
 		{
-			return;
+			postorder(root.get(), std::move(visit_action));
 		}
 
-		postorder(n->left_child.get(), std::move(visit_action));
-		postorder(n->right_child.get(), std::move(visit_action));
-		visit_action(n->data);
-	}
-
-	template <class T>
-	void binary_tree<T>::inorder(bin_tree::node<T>* n, std::function<void(T& data)>&& visit_action)
-	{
-		if (n == nullptr)
+		template <class T>
+		void binary_tree<T>::inorder(std::function<void(T &data)> visit_action)
 		{
-			return;
+			inorder(root.get(), std::move(visit_action));
 		}
 
-		inorder(n->left_child.get(), std::move(visit_action));
-		visit_action(n->data);
-		inorder(n->right_child.get(), std::move(visit_action));
-	}
-
-	template <class T>
-	long long binary_tree<T>::height()
-	{
-		return height(this->root);
-	}
-
-	template <class T>
-	long long binary_tree<T>::height(const std::shared_ptr<bin_tree::node<T>>& n)
-	{
-		if (n == nullptr)
+		template <class T>
+		void binary_tree<T>::preorder(node<T>* n, std::function<void(T& data)> &&visit_action)
 		{
-			return 0;
+			if (n == nullptr)
+			{
+				return;
+			}
+
+			visit_action(n->data);
+			preorder(n->left_child.get(), std::move(visit_action));
+			preorder(n->right_child.get(), std::move(visit_action));
 		}
 
-		auto left_height = height(n->left_child);
-		auto right_height = height(n->right_child);
+		template <class T>
+		void binary_tree<T>::postorder(node<T>* n, std::function<void(T& data)>&& visit_action)
+		{
+			if (n == nullptr)
+			{
+				return;
+			}
 
-		return std::max(left_height, right_height) + 1;
+			postorder(n->left_child.get(), std::move(visit_action));
+			postorder(n->right_child.get(), std::move(visit_action));
+			visit_action(n->data);
+		}
+
+		template <class T>
+		void binary_tree<T>::inorder(node<T>* n, std::function<void(T& data)>&& visit_action)
+		{
+			if (n == nullptr)
+			{
+				return;
+			}
+
+			inorder(n->left_child.get(), std::move(visit_action));
+			visit_action(n->data);
+			inorder(n->right_child.get(), std::move(visit_action));
+		}
+
+		template <class T>
+		long long binary_tree<T>::height()
+		{
+			return height(this->root);
+		}
+
+		template <class T>
+		long long binary_tree<T>::height(const std::shared_ptr<node<T>>& n)
+		{
+			if (n == nullptr)
+			{
+				return 0;
+			}
+
+			auto left_height = height(n->left_child);
+			auto right_height = height(n->right_child);
+
+			return std::max(left_height, right_height) + 1;
+		}
 	}
 }
