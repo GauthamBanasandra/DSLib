350fe6d64c3499e2685448e6bce26039ca5b302f
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-12-16 19:55:18 +0530

    Fix bug with node removal
    * root node that was being passed to replace was just getting modified locally. We're now passing a reference.

--- DSLib/Lib/BinaryTree/Node.h
+++ DSLib/Lib/BinaryTree/Node.h
@@ -18,9 +18,9 @@ namespace ds
 			}
 
 			bool is_leaf() { return left_child == nullptr && right_child == nullptr; }
-			void replace(node<T> *root, node<T> *other);
-			void copy_data_to(node<T> * other) { other->data = this->data; }			
-			static void remove(node<T> *other);			
+			void replace(node<T> *&root, node<T> *other);
+			void copy_data_to(node<T> * other) { other->data = this->data; }
+			static void remove(node<T> *other);
 
 			T data;
 			long long height;
@@ -34,7 +34,7 @@ namespace ds
 		// This doesn't handle childrens' links which is the responsibility of the caller
 		// TODO : Move this method to bst class to avoid passing root as parameter
 		template <typename T>
-		void node<T>::replace(node<T> * root, node<T> * other)
+		void node<T>::replace(node<T> * &root, node<T> * other)
 		{
 			auto &ancestor = this->ancestor;
 			switch (this->node_type)
@@ -53,7 +53,9 @@ namespace ds
 			}
 
 			other->ancestor = ancestor;
-			other->node_type = this->node_type;			
+			other->node_type = this->node_type;
+
+			delete this;
 		}
 
 		template <typename T>
