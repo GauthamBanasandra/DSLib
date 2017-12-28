1f3ae4af9a6cb0b5c0f8fb8b33132ce627e44255
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-12-28 22:08:31 +0530

    Add random data test Segment tree

--- DSLib/Test/SegmentTree/SegmentTree.cpp
+++ DSLib/Test/SegmentTree/SegmentTree.cpp
@@ -29,8 +29,8 @@ void extensive_query(const std::vector<T> &data, ds::bin_tree::seg_tree<T> &seg_
 			auto expected = data[seg_tree_gold.rmq(i, j)];
 			auto actual = seg_tree.query(query_segment);
 
-			std::string msg = "Min[" + std::to_string(i) + ", " + std::to_string(j) + "] = exp: " + std::to_string(expected) + "\tact: " + std::to_string(actual.data);
-			Logger::WriteMessage(msg.c_str());
+			/*std::string msg = "Min[" + std::to_string(i) + ", " + std::to_string(j) + "] = exp: " + std::to_string(expected) + "\tact: " + std::to_string(actual.data);
+			Logger::WriteMessage(msg.c_str());*/
 			Assert::IsTrue(actual.is_valid, L"Query must be valid");
 			Assert::AreEqual(expected, actual.data);
 		}
@@ -38,11 +38,17 @@ void extensive_query(const std::vector<T> &data, ds::bin_tree::seg_tree<T> &seg_
 }
 
 template<class T>
-void extensive_update(std::vector<T> &data, ds::bin_tree::seg_tree<T> &seg_tree, SegmentTreeGold& seg_tree_gold, T value)
+void extensive_update(std::vector<T> &data, ds::bin_tree::seg_tree<T> &seg_tree, SegmentTreeGold& seg_tree_gold, T new_value, bool random)
 {
+	auto value = new_value;
 	ds::bin_tree::range update_segment;
 	for (std::size_t i = 0; i < data.size(); ++i)
 	{
+		if (random)
+		{
+			value = rand();
+		}
+
 		update_segment.lower_bound = update_segment.upper_bound = i;
 		seg_tree.update_range(update_segment, value, ds::bin_tree::update_mode::k_memoryless);
 		seg_tree_gold.update_point(i, value);
@@ -51,6 +57,23 @@ void extensive_update(std::vector<T> &data, ds::bin_tree::seg_tree<T> &seg_tree,
 	}
 }
 
+std::vector<int> generate_data(const std::size_t data_size)
+{
+	std::vector<int> data;
+	const auto seed = static_cast<unsigned>(time(nullptr));
+
+	// Log the seed
+	auto msg = "Seed: " + std::to_string(seed);
+	Logger::WriteMessage(msg.c_str());
+
+	srand(seed);
+	for (std::size_t i = 0; i < data_size; ++i)
+	{
+		data.push_back(rand());
+	}
+
+	return data;
+}
 namespace SegmentTree
 {
 	TEST_CLASS(seg_tree)
@@ -71,7 +94,29 @@ namespace SegmentTree
 			ds::bin_tree::seg_tree<int> seg_tree(&data, data.size(), access_data<int>, merge_nodes<int>);
 			SegmentTreeGold seg_tree_gold(data);
 
-			extensive_update(data, seg_tree, seg_tree_gold, 1);
+			extensive_update(data, seg_tree, seg_tree_gold, 1, false);
+		}
+	};
+
+	TEST_CLASS(random_data)
+	{
+	public:
+		TEST_METHOD(random_query_test)
+		{
+			auto data = generate_data(1000);
+			ds::bin_tree::seg_tree<int> seg_tree(&data, data.size(), access_data<int>, merge_nodes<int>);
+			SegmentTreeGold seg_tree_gold(data);
+
+			extensive_query(data, seg_tree, seg_tree_gold);
+		}
+
+		TEST_METHOD(random_update_test)
+		{
+			auto data = generate_data(20);
+			ds::bin_tree::seg_tree<int> seg_tree(&data, data.size(), access_data<int>, merge_nodes<int>);
+			SegmentTreeGold seg_tree_gold(data);
+
+			extensive_update(data, seg_tree, seg_tree_gold, 0, true);
 		}
 	};
 }
--- DSLib/Test/SegmentTree/stdafx.h
+++ DSLib/Test/SegmentTree/stdafx.h
@@ -14,3 +14,4 @@
 #include "SegmentTreeGold.h"
 
 #include <vector>
+#include <time.h>
