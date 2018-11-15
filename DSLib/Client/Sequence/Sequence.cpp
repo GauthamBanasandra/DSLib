0db059c9d6cb15f3ef6de35a87d9cdfa3b066088
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-11-15 23:05:49 +0530

    [Sequence] Add "Longest increasing subsequence latest elements"

--- DSLib/Client/Sequence/Sequence.cpp
+++ DSLib/Client/Sequence/Sequence.cpp
@@ -4,16 +4,21 @@
 
 int main(int argc, char* argv[])
 {
-	const std::vector<int> v{
-		//-7, 10, 9, 2, 3, 8, 8, 6 
-		//6, 1, 5, 4, 7, 3, 2, 9, 8, 10
-		//15,27,14,38,26,55,46,65,85
-		2, 7, 4, 3, 8
-	};
-	for (auto number : ds::seq::longest_increasing(v))
+	const std::vector<int> numbers{ 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
+
+	for (auto number : ds::seq::longest_increasing_latest_elements(numbers))
 	{
 		std::cout << number << " ";
 	}
 	std::cout << std::endl;
+
+	/*const std::vector<std::size_t> lis_indices{ 0, 2, 3 };
+	const auto find_it = std::lower_bound(lis_indices.begin(), lis_indices.end(), 2, [&numbers](const std::size_t &i_lis, const int &number) -> bool
+	{
+		std::cout << "i_lis : " << i_lis << "\tnumbers[i_lis] : " << numbers[i_lis] << "\tnumber : " << number << std::endl;
+		return numbers[i_lis] < number;
+	});
+
+	std::cout << *find_it << std::endl;*/
 	return 0;
 }
--- DSLib/Lib/Sequence/Sequence.h
+++ DSLib/Lib/Sequence/Sequence.h
@@ -9,9 +9,55 @@ namespace ds
 	{
 	public:
 		template<typename T>
+		static std::vector<T> longest_increasing_latest_elements(const std::vector<T> &numbers);
+
+		template<typename T>
 		static std::vector<T> longest_increasing(const std::vector<T> &numbers);
 	};
 
+	// This method returns the longest increasing sequence in which the elements
+	// are as new as possible
+	// Try running it against the following input 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15
+	template <typename T>
+	std::vector<T> seq::longest_increasing_latest_elements(const std::vector<T>& numbers)
+	{
+		const auto numbers_len = numbers.size();
+		std::vector<std::size_t> p(numbers_len);
+
+		std::vector<T> lis;
+		lis.reserve(numbers_len);
+
+		std::vector<std::size_t> m;
+		m.reserve(numbers_len + 1);
+		m.emplace_back(0);
+
+		for (std::size_t i = 0; i < numbers_len; ++i)
+		{
+			auto find_it = std::lower_bound(m.begin() + 1, m.end(), numbers[i],
+				[&numbers](const std::size_t &i_m, const T &number)->bool { return numbers[i_m - 1] < number; });
+			auto i_m = std::distance(m.begin(), find_it);
+			if (find_it == m.end())
+			{
+				m.emplace_back(i + 1);
+			}
+			else
+			{
+				*find_it = i + 1;
+			}
+			p[i] = m[i_m - 1];
+		}
+
+		for (auto i = m[m.size() - 1]; i > 0; i = p[i - 1])
+		{
+			lis.emplace_back(numbers[i - 1]);
+		}
+		std::reverse(lis.begin(), lis.end());
+		return lis;
+	}
+
+	// This method returns the longest increasing subsequence in which 
+	// the elements are older in the order that they appear
+	// Try running this against the input 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15
 	template <typename T>
 	std::vector<T> seq::longest_increasing(const std::vector<T>& numbers)
 	{
